3a7dd2c561d3d7643840841defe4c2ec9b5d5cde
Alex Wan <haichaow@sfu.ca>
2019-06-10 14:38:24 -0700

    assignment2

--- Assignment2/server.js
+++ Assignment2/server.js
@@ -6,11 +6,10 @@ const path = require('path');
 const PORT = process.env.PORT || 3002;
 const {Pool} = require('pg');
 var pool = new Pool({
-	user: 'vixuaezwojkhju',
-	password:'04dffbeeacb64cb1f66573f8a3df82a7ac3a239e3660053960fd587303b5cac1',
-	host:'ec2-23-21-186-85.compute-1.amazonaws.com',
-	database:'de5jas1v51563d',
-	port:'5432'
+	user: 'postgres',
+	password:'root',
+	host:'localhost',
+	database:'postgres',
 });
 
 app.use('/', express.static('./public'));
